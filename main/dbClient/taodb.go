65417a7d79d13924c92d36ed022c999665db33a4
tang <tsandxxx@gmail.com>
2022-01-14 19:32:57 +0800

    commit readme

--- main/dbClient/taodb.go
+++ main/dbClient/taodb.go
@@ -71,7 +71,7 @@ func main() {
 	fmt.Printf("cost=[%s]", cost)
 	data2, _ := client.Get("picture2")
 	operator.WriteFile("F:\\code\\go_code\\db1\\file\\result\\picture999.jpg", data2)
-
+	// 需要將上傳的所有文件名記錄一下，這樣從minio中讀取時知道讀取什麽
 	//for i := 0; i < 100; i++ {
 	//	value, _ := client.Get(fmt.Sprintf("hello%d", i))
 	//	log.Info("get key:hello%d,value=%s\n", i, string(value[:]))
